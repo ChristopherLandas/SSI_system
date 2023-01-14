f6849ff4760e052cb04a8bce591225633f6f16d2
ChristopherLandas <103894996+ChristopherLandas@users.noreply.github.com>
2023-01-14 10:12:34 +0800

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,14 @@
+class db_constants:
+    DB = 'salary_management_system'
+
+class db_login_credentials_constants:
+    TABLE = 'login_credentials'
+    SALT = 'slt'
+    USERNAME = 'usn'
+    PASSWORD = 'pss'
+
+class db_account_information:
+    FULL_NAME = 'full_name'
+    BIRTH_DATE = 'bday'
+    SALARY = 'salary'
+    FOREIGN_USN = 'usn'
--- login.py
+++ login.py
@@ -1,11 +1,10 @@
-import customtkinter as ctk
-import datetime
 from tkextrafont import Font
 from tkcalendar import Calendar
 from PIL import Image
+import customtkinter as ctk
+from constants import *
 import mariadb
 import tkinter
-from Constants import db_constants
 import hashlib
 import re
 
@@ -15,7 +14,8 @@ ctk.set_default_color_theme("blue")
 class login_GUI(ctk.CTk):
     def __init__(self):
         super().__init__()
-        dbKey = db_constants.login_credential_columns
+        DBKEY = db_login_credentials_constants
+        DB_CONS = db_constants
         def signin():
             '''steps:
             get salt
@@ -29,13 +29,14 @@ class login_GUI(ctk.CTk):
             try:
                 db_con = mariadb.connect(user="root", password="test123", host="127.0.0.1", port=3306, database='test')
                 db_cur = db_con.cursor()
-                db_cur.execute(f'SELECT {dbKey.SALT} FROM {dbKey.TABLE} WHERE {dbKey.USERNAME} = ?', (self.email_entry.get(), ))
+                db_cur.execute(f'SELECT {DBKEY.SALT} FROM {DBKEY.TABLE} WHERE {DBKEY.USERNAME} = ?', (self.email_entry.get(), ))
                 salt = str(db_cur.fetchall()[0][0]).encode('utf-8')
             except mariadb.Error:
                 tkinter.messagebox.showinfo('db error', 'db error')
                 return
             except IndexError:
-                tkinter.messagebox.showinfo('incorrect', 'incorrect')
+                self.invalid_label.configure(text='Invalid entry email or password')
+                #tkinter.messagebox.showinfo('incorrect', 'incorrect')
             else:
                 db_cur.close()
                 db_con.close()
@@ -54,7 +55,7 @@ class login_GUI(ctk.CTk):
             try:
                 db_con = mariadb.connect(user="root", password="test123", host="127.0.0.1", port=3306, database='test')
                 db_cur = db_con.cursor()
-                db_cur.execute(f'SELECT COUNT(*) AS len FROM {dbKey.TABLE} WHERE {dbKey.USERNAME} = ? AND {dbKey.PASSWORD} = ?', (self.email_entry.get(), initial_pass))
+                db_cur.execute(f'SELECT COUNT(*) AS len FROM {DBKEY.TABLE} WHERE {DBKEY.USERNAME} = ? AND {DBKEY.PASSWORD} = ?', (self.email_entry.get(), initial_pass))
                 does_have = db_cur.fetchall()[0][0] > 0
             except mariadb.Error:
                 tkinter.messagebox.showinfo('db error', 'db error')
@@ -67,7 +68,8 @@ class login_GUI(ctk.CTk):
                 #main_page.main_page(self, withdraw_root=True)
                 tkinter.messagebox.showinfo('', 'login')
             else:
-                tkinter.messagebox.showinfo('', 'incorrect')
+                self.invalid_label.configure(text='Invalid entry email or password')
+                #tkinter.messagebox.showinfo('', 'incorrect')
 
 
         self.title("Application Development")
@@ -114,9 +116,9 @@ class login_GUI(ctk.CTk):
         self.password_entry = ctk.CTkEntry(self.entry_containter, width=280, height=40,show="*", placeholder_text="Enter Password")
         self.password_entry.pack(fill="x", pady=10, padx=10)
 
-        self.invalid_label = ctk.CTkLabel(self.entry_containter, text="Invalid entry email or password", font=("Lucida Console",12), text_color="red")
+        self.invalid_label = ctk.CTkLabel(self.entry_containter, text="", font=("Lucida Console",12), text_color="red")
         self.invalid_label.pack()
-        self.invalid_label.pack_forget()
+        #self.invalid_label.pack_forget() -> change the text instead of fogetting the fact
 
         self.create_account_btn = ctk.CTkButton(self.entry_containter,command=self.create_event, width=150, height=30,  text="Create Account", font=("Lucida Console", 14),fg_color="#00ADB5", border_color="#393E46")
         self.create_account_btn.pack(side="bottom",pady=(5,30))
@@ -139,27 +141,34 @@ class login_GUI(ctk.CTk):
             self.create_account_btn.configure(state="normal")
 
         def check_entry():
-            if self.fname_entry.get() and self.lname_entry.get():
-                if self.bday_textlabel.cget("text") != "yyyy/mm/dd":
-                    if self.emailCreate_entry.get():
-                        if self.passCreate_entry.get() and self.confirmPassCreate_entry.get():
-                            if self.passCreate_entry.get() == self.confirmPassCreate_entry.get():
-                                self.error_msg.place_forget()
-                                self.create_acc.destroy()
-                                self.create_account_btn.configure(state="normal")
-                                print("proceed")
+            global DB_CONS
+            '''
+            if (self.fname_entry.get() and self.lname_entry.get()): #check if the first and last name entries were filled
+                if self.bday_textlabel.cget("text") != "yyyy/mm/dd": #check if there's birthdate
+                    if self.emailCreate_entry.get(): #check the email entry if it was filled
+                        if re.match(r'^([\w]{3})+(\.[\w]+)*@[\w\.]+(com|edu|net|gov|org)(\.\D{2})*$', self.emailCreate_entry.get()): #check if the email is in valid format
+                            if self.passCreate_entry.get() and self.confirmPassCreate_entry.get(): # check if the password entries were filled
+                                if self.passCreate_entry.get() == self.confirmPassCreate_entry.get(): #check if both password and re-type password were filled
+                                    self.error_msg.place_forget()
+                                    self.create_acc.destroy()
+                                    self.create_account_btn.configure(state="normal")
+                                    print("proceed")
+                                else:
+                                    self.error_msg.configure(text="Password do not match")
+                                    self.error_msg.place(relx=0.5, rely=0.87, anchor="c")
+                                pass
                             else:
-                                self.error_msg.configure(text="Password do not match")
+                                self.error_msg.configure(text="Enter Password")
                                 self.error_msg.place(relx=0.5, rely=0.87, anchor="c")
                             pass
                         else:
-                            self.error_msg.configure(text="Enter Password")
+                            self.error_msg.configure(text="Invalid Email Format")
                             self.error_msg.place(relx=0.5, rely=0.87, anchor="c")
                         pass
                     else:
                         self.error_msg.configure(text="Enter Email")
                         self.error_msg.place(relx=0.5, rely=0.87, anchor="c")
-                    pass
+                        pass
                 else:
                     self.error_msg.configure(text="Enter birthday")
                     self.error_msg.place(relx=0.5, rely=0.87, anchor="c")
@@ -167,6 +176,53 @@ class login_GUI(ctk.CTk):
             else:
                 self.error_msg.configure(text="Complete name fields")
                 self.error_msg.place(relx=0.5, rely=0.87, anchor="c")
+            '''
+            print(check_name_format(self.fname_entry.get()), check_name_format(self.lname_entry.get()))
+            warning_text = ''
+            if not (self.fname_entry.get() and self.lname_entry.get()):
+                warning_text = 'Complete name fields'
+                #check if the first and last name entries were filled
+            elif (check_name_format(self.fname_entry.get()) or check_name_format(self.lname_entry.get())):
+                warning_text = 'Invalid name format'
+            elif self.bday_textlabel.cget("text") == "yyyy/mm/dd":
+                warning_text = 'Enter birthday'
+                #check if there's birthdate
+            elif not self.emailCreate_entry.get():
+                warning_text = 'Enter Email'
+                #check the email entry if it was filled
+            elif not check_email_format(self.emailCreate_entry.get()):
+                print(self.emailCreate_entry.get())
+                warning_text = 'Invalid Email Format'
+                #check if the email is in valid format
+            elif not (self.passCreate_entry.get() and self.confirmPassCreate_entry.get()):
+                warning_text = 'Enter Password'
+                # check if the password entries were filled
+            if self.passCreate_entry.get() != self.confirmPassCreate_entry.get():
+                warning_text = 'Password did not match'
+                #check if both password and re-type password were filled
+
+            if(warning_text != ''):
+                self.error_msg.configure(text=warning_text)
+                self.error_msg.place(relx=0.5, rely=0.87, anchor="c")
+                return
+
+            self.error_msg.place_forget()
+            self.create_acc.destroy()
+            self.create_account_btn.configure(state="normal")
+            '''
+            try:
+                db_con = mariadb.connect(user="root", password="test123", host="127.0.0.1", port=3306, database= DB_CONS.DB)
+                db_cur = db_con.cursor()
+                db_cur.execute('Insert Into login_credentials values (?, ?, ?)', (self.emailCreate_entry, self.passCreate_entry, ))
+            except mariadb.Error:
+                warning_text = 'Error connecting to db'
+            ''' #bukas na tinatamad na ko
+
+        def check_email_format(email):
+            return bool(re.match(r'^([\w]{3})+(\.[\w]+)*@[\w\.]+(com|edu|net|gov|org)(\.[a-z]{2})*$', email, re.IGNORECASE))
+        def check_name_format(name):
+            print(len(re.findall(r'[\d]+', name, re.IGNORECASE)))
+            return bool(len(re.findall(r'[\d]+', name, re.IGNORECASE)) > 0)
 
         self.create_account_btn.configure(state="disabled")
         win_width = 450
