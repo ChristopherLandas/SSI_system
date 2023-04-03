3fb337e678d55cecc1cd75c8c303805231b81953
Christopher Landas <Landas.229246@fairview.sti.edu.ph>
2023-04-03 15:59:02 +0800

    Fix Btn Manager, recode side bar and top bar btns

--- customcustomtkinter.py
+++ customcustomtkinter.py
@@ -2,7 +2,6 @@ import customtkinter as ctk
 import tkinter as tk
 from typing import *
 from functools import partial
-from functools import wraps
 from util import brighten_color
 
 class customcustomtkinter:
@@ -20,18 +19,28 @@ class customcustomtkinter:
 
             self.og_color = self._fg_color
             self._hover_color = hover_color
-            self._command = command
-            self._commands = [command]
+            self._command = [] if isinstance(command, list) else [command]
             self._hover = hover
-            #make a reference/encapsulation from extended arguents
+            #make a reference/encapsulation from extended arguments
 
-            self.bind('<Button-1>', command)
+            self.bind('<Button-1>', self.response)
             self.pack_propagate(0)
             self.grid_propagate(0)
-            self.update_button()
-            #set the sequence events of 3rd party properties
+            self.update_button(hover)
+            #set the property as a button
 
-        def update_button(self, is_hover = True):
+        def response(self, _):
+                click_color = (brighten_color(self._hover_color[0], 1.25, brighten_color(self._hover_color[1], 1.25))) if isinstance(self._hover_color, tuple) else brighten_color(self._hover_color, 1.45)
+                self.configure(fg_color = click_color)
+                self.update()
+                self.after(50)
+                self.configure(fg_color = self._hover_color)
+                for cmd in self._command:
+                    if(callable(cmd)):
+                        cmd()
+        #click response of the button; includes the flash when clicked
+
+        def update_button(self, is_hover: bool = True):
             if(is_hover):
                 self.bind('<Enter>', lambda _: self.configure(fg_color = self._hover_color))
                 self.bind('<Leave>', lambda _: self.configure(fg_color = self.og_color))
@@ -44,8 +53,7 @@ class customcustomtkinter:
             child = self.winfo_children()
             for i in child:
                 i.unbind('<Button-1>', None)
-                for c in self._commands:
-                    i.bind('<Button-1>', c)
+                i.bind('<Button-1>', self.response)
                 if(self._hover):
                     i.bind('<Enter>', lambda _: self.configure(fg_color = self._hover_color))
                     i.bind('<Leave>', lambda _: self.configure(fg_color = self.og_color))
@@ -57,11 +65,9 @@ class customcustomtkinter:
         def configure(self, require_redraw=False, command: Optional[Callable[[], None]] = None, hover: Optional[bool] = None,
                       hover_color: Optional[Union[str, Tuple[str, str]]] = None, **kwargs):
             if(command is not None):
-                self._command = command
-                self._commands.append(command)
+                self._command = [] if isinstance(command, list) else [command]
                 self.unbind('<Button-1>', None)
-                for c in self._commands:
-                    self.bind('<Button-1>', c)
+                self.bind('<Button-1>', self.response)
                 self.update_children()
             if(hover_color is not None):
                 self._hover_color = hover_color
@@ -73,37 +79,86 @@ class customcustomtkinter:
                 self.update_children()
             return super().configure(require_redraw, **kwargs)
         #override configure function of frame, allowing to add those external arguments
+    #button frame: a frame with a properties of a buttons
 
+    class menubar(ctk.CTkFrame):
+        def __init__(self, master: any, width: int = 200, height: int = 200, corner_radius: Optional[Union[int, str]] = None,
+                     border_width: Optional[Union[int, str]] = None, bg_color: Union[str, Tuple[str, str]] = "transparent",
+                     fg_color: Optional[Union[str, Tuple[str, str]]] = None, border_color: Optional[Union[str, Tuple[str, str]]] = None,
+                     background_corner_colors: Union[Tuple[Union[str, Tuple[str, str]]], None] = None,
+                     overwrite_preferred_drawing_method: Union[str, None] = None, position: tuple = (0, 0, 'c'), **kwargs):
+            super().__init__(master, width, height, corner_radius, border_width, bg_color, fg_color, border_color, background_corner_colors,
+                             overwrite_preferred_drawing_method, **kwargs)
+
+            self._position = position
+            self._shadow = ctk.CTkFrame(master, 0, 0)
+            self.pack_propagate(0)
+            self.grid_propagate(0)
+
+        def place(self):
+            #place shadow here
+            return super().place(relx = self._position[0], rely = self._position[1], anchor = self._position[2])
+        #override the place to place a shadow first
+
+        def update_pos(self,  position: tuple = (0, 0, 'c')):
+            self.deiconify()
+            self._position = position
+            self.place()
+        #update the position, including the shadow
+
+        def deiconify(self):
+            self._shadow.place_forget()
+            self.place_forget()
+        #hide the menubar
+
+class customcustomtkinterutil:
     class button_manager:
-        def __init__(self, buttons: list = None, hold_color: str = 'transparent', og_color: str = 'green',
-                     default_active: Optional[int] = None, commands: tuple = (lambda: print('disable'), lambda: print('enable'))):
+        def __init__(self, buttons: list, hold_color: str = 'transparent', switch: bool = False,
+                     default_active: Optional[int] = None, state: tuple = (lambda: None, lambda: None), children: Optional[list] = None):
             self.active = None
-            self._command = commands
-            self._og_color = og_color
+            self._state = state
+            self._og_color =[]
+            self._switch = switch
             self._hold_color = hold_color
             self._buttons = buttons
             self._default_active = default_active
+            self._children = children
             #setup variables
 
             for i in range(len(buttons)):
-                buttons[i].bind('<Button-1>', partial(self.click, i))
                 if(isinstance(buttons[i], customcustomtkinter.ctkButtonFrame)):
-                    buttons[i].configure(command = partial(self.click, i))
+                    buttons[i]._command.append(partial(self.click, i))
+                elif(isinstance(buttons[i], ctk.CTkButton)):
+                    cmd = buttons[i]._command
+                    buttons[i].configure(command = partial(self.click, i, cmd))
+                self._og_color.append(buttons[i]._fg_color)
+            #set the designated command for buttonframe and ctkbutton
 
-        def click(self, i: int, _):
-            if(isinstance(self._buttons[i], customcustomtkinter.ctkButtonFrame)):
-                og = self._buttons[i]._fg_color
-                click_color = brighten_color(og, 1.25) if isinstance(og, tuple) else (brighten_color(og, 1.25), brighten_color(og, .75))
-                self._buttons[i].configure(fg_color = click_color)
-                self._buttons[i].update()
-                self._buttons[i].after(50, None)
+        def click(self, i: int, button_command: callable = None, e: any = None):
+            if(button_command is not None):
+                button_command()
+            #actual command of a button
 
-            if(self.active is not None):
-                self.active.configure(fg_color = self._og_color)
+            if(self.active == self._buttons[i] and self._switch):
+                if(self._children is not None):
+                    self._children[i].deiconify()
                 self.active.configure(hover = True)
-                self._command[0]()
+                self.active.configure(fg_color = self._og_color[i])
+                self._state[0]()
+                self.active = None
+                return
+            #if the clicked button was the active button and the switch mode is on
+            elif(self.active is not None):
+                if(self._children is not None):
+                    self._children[self._buttons.index(self.active)].deiconify()
+                self.active.configure(fg_color = self._og_color[self._buttons.index(self.active)])
+                self.active.configure(hover = True)
+                self._state[0]()
+            #if theres an existing active button
+            if(self._children is not None):
+                self._children[i].place()
             self.active = self._buttons[i]
             self.active.configure(hover = False)
             self.active.configure(fg_color = self._hold_color)
-            self._command[1]()
+            self._state[1]()
         #setup click variable
--- dashboard.py
+++ dashboard.py
@@ -9,6 +9,7 @@ from Theme import Color
 from PIL import Image
 from datetime import date
 from customcustomtkinter import customcustomtkinter as cctk
+from customcustomtkinter import customcustomtkinterutil as cctku
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
@@ -69,25 +70,7 @@ class dashboard(ctk.CTkToplevel):
         self.active_main_frame = None
 
         '''events'''
-        '''
-        def change_active_event(cur, cur_frame):
-            if(self.active_win is not None):
-                self.active_win.configure(fg_color = unselected_btn_color)
-                self.active_win.winfo_children()[0].configure(fg_color="transparent")
-                self.active_win.configure(hover=True)
-            if(self.active_main_frame is not None):
-                self.active_main_frame.grid_forget()
-            self.active_main_frame = self.main_frames[cur_frame]
-            self.main_frames[cur_frame].grid(row=1, column=1, sticky="nsew")
-            self.active_win = cur
-            #print(self.active_win.winfo_children()[0])
-            self.active_win.winfo_children()[0].configure(fg_color=Color.White_Ghost)
-            self.title_label.configure(text=f"{self.active_win.winfo_children()[2].cget('text').strip().upper()}")
-            cur.configure(fg_color = selected_btn_color)
-            cur.configure(hover=False)
-        '''
-
-        def load_main_frame(title: str, cur_frame: int, e):
+        def load_main_frame(title: str, cur_frame: int):
             self.title_label.configure(text= title.upper())
             if self.active_main_frame is not None:
                 self.active_main_frame.grid_forget()
@@ -98,81 +81,8 @@ class dashboard(ctk.CTkToplevel):
         def switch_darkmode():
             print(switch_var_darkmode.get())
 
-        '''menubars'''
-        self.current_opened_menu_bar = None
-        self.notif_menu_bar = None
-        self.settings_menu_bar = None
-        self.acc_menu_bar = None
-        self.active_btn_menu_bar = None
-
-        def show_notif_menubar():
-            if(self.current_opened_menu_bar is not None):
-                self.current_opened_menu_bar.destroy()
-                self.active_btn_menu_bar.configure(fg_color=Color.White_Ghost)
-                self.active_btn_menu_bar = None
-                if(str(self.current_opened_menu_bar) == str(self.notif_menu_bar)):
-                    self.current_opened_menu_bar = None
-                    return
-            self.notif_btn.configure(fg_color = Color.White_Platinum)
-            self.notif_menu_bar= ctk.CTkFrame(self, width * default_menubar_width, height * default_menubar_height,
-                                        corner_radius= 0, fg_color='black', border_width= 0)
-            self.notif_menu_bar.place(relx = self.notif_btn.winfo_rootx() / self.winfo_width() + default_menubar_width/2,
-                                rely= self.top_frame.winfo_height()/ self.winfo_height() + default_menubar_height/2,
-                                anchor = 'c')
-            self.active_btn_menu_bar = self.notif_btn
-
-            self.current_opened_menu_bar = self.notif_menu_bar
-            '''content code here'''
-
         '''Switch Value'''
         switch_var_darkmode = ctk.StringVar(value="lightmode")
-        def show_settings_menubar():
-            if(self.current_opened_menu_bar is not None):
-                self.current_opened_menu_bar.destroy()
-                self.active_btn_menu_bar.configure(fg_color=Color.White_Ghost)
-                self.active_btn_menu_bar = None
-                if(str(self.current_opened_menu_bar) == str(self.settings_menu_bar)):
-                    self.current_opened_menu_bar = None
-                    self.settings_btn.configure(fg_color=Color.White_Ghost)
-                    return
-            self.settings_btn.configure(fg_color=Color.White_Platinum)
-            self.settings_menu_bar = ctk.CTkFrame(self, width * default_menubar_width, height * default_menubar_height,
-                                                    corner_radius= 5, fg_color=Color.White_Ghost, border_width= 0)
-            self.settings_menu_bar.pack_propagate(0)
-            self.settings_menu_bar.place(relx= self.settings_btn.winfo_rootx() / self.winfo_width() + default_menubar_width/2,
-                                        rely = self.top_frame.winfo_height()/ self.winfo_height() + default_menubar_height/2+0.005,
-                                        anchor = 'c')
-            self.active_btn_menu_bar = self.settings_btn
-
-
-            self.settings_menu_bar_dark_mode = ctk.CTkSwitch(self.settings_menu_bar,width=round(width * default_menubar_width/2), height=round(height * .12),text="Dark Mode",
-                                                             font=("Poppins Medium", 16), progress_color=Color.Blue_LapisLazuli_1, text_color=Color.Blue_Maastricht,
-                                                             variable = switch_var_darkmode, onvalue="darkmode", offvalue="lightmode",
-                                                             command=switch_darkmode,)
-            self.settings_menu_bar_dark_mode.pack(anchor= 'c')
-            '''content code here'''
-            self.current_opened_menu_bar = self.settings_menu_bar
-
-        def show_acc_menubar(_):
-            if(self.current_opened_menu_bar is not None):
-                self.current_opened_menu_bar.destroy()
-                self.active_btn_menu_bar.configure(fg_color=Color.White_Ghost)
-                self.active_btn_menu_bar = None
-                self.acc_btn.configure(hover= True)
-                if(str(self.current_opened_menu_bar) == str(self.acc_menu_bar)):
-                    self.current_opened_menu_bar = None
-                    self.acc_btn.configure(fg_color=Color.White_Gray)
-                    return
-            self.acc_btn.configure(hover= False, fg_color=Color.White_Platinum)
-            self.acc_menu_bar = ctk.CTkFrame(self, width * acc_menubar_width, height * default_menubar_height, 0, fg_color=Color.White_Ghost)
-            self.acc_menu_bar.pack_propagate(0)
-            self.acc_menu_bar.place(relx = 1 - acc_menubar_width/2,
-                                    rely= self.top_frame.winfo_height()/ self.winfo_height() + default_menubar_height/2,
-                                    anchor = 'c')
-
-            self.active_btn_menu_bar = self.acc_btn
-            self.current_opened_menu_bar = self.acc_menu_bar
-            return
 
         self.grid_rowconfigure(1,weight=1)
         self.side_frame = ctk.CTkFrame(self, height= height, width = side_frame_w,
@@ -335,11 +245,11 @@ class dashboard(ctk.CTkToplevel):
         self.histlog_button.pack()
         self.histlog_button.update_children()
 
-        self.sidebar_btn_mngr = cctk.button_manager([self.db_button, self.transact_button, self.services_button, self.sales_button,
+        self.sidebar_btn_mngr = cctku.button_manager([self.db_button, self.transact_button, self.services_button, self.sales_button,
                                                      self.inventory_button, self.patient_button, self.report_button, self.user_setting_button,
-                                                     self.histlog_button], selected_btn_color, unselected_btn_color, 0)
-        self.sidebar_btn_mngr._command = (lambda: self.sidebar_btn_mngr.active.winfo_children()[0].configure(fg_color="transparent"),
-                                          lambda: self.sidebar_btn_mngr.active.winfo_children()[0].configure(fg_color=Color.White_Ghost))
+                                                     self.histlog_button], selected_btn_color, False, 0)
+        self.sidebar_btn_mngr._state = (lambda: self.sidebar_btn_mngr.active.winfo_children()[0].configure(fg_color="transparent"),
+                                        lambda: self.sidebar_btn_mngr.active.winfo_children()[0].configure(fg_color=Color.White_Ghost))
         self.sidebar_btn_mngr.click(self.sidebar_btn_mngr._default_active, None)
 
 
@@ -357,41 +267,64 @@ class dashboard(ctk.CTkToplevel):
 
         self.notif_btn = ctk.CTkButton(master= self.top_frame, width= round(self.top_frame.winfo_reqheight()*0.5), text= "", image= self.notif_icon,
                                               fg_color=Color.White_Ghost, height= round(self.top_frame.winfo_reqheight() *0.5), border_width=0, corner_radius=5,
-                                              font=("Poppinds Medium", 16),hover_color=Color.White_Gray,
-                                              command= show_notif_menubar,)
+                                              font=("Poppinds Medium", 16),hover_color=Color.White_Gray,)
         self.notif_btn.grid(row=0, column= 1, sticky='w')
         self.settings_btn = ctk.CTkButton(master= self.top_frame, width= round(self.top_frame.winfo_reqheight()* 0.5), text= "", image= self.settings_icon,
                                               fg_color=Color.White_Ghost, height= round(self.top_frame.winfo_reqheight()* 0.5), border_width=0, corner_radius=5,
-                                              font=("Poppinds Medium", 16),hover_color=Color.White_Gray,
-                                              command= show_settings_menubar)
+                                              font=("Poppinds Medium", 16),hover_color=Color.White_Gray,)
         self.settings_btn.grid(row=0, column= 2, sticky='w')
 
         self.acc_btn = cctk.ctkButtonFrame(self.top_frame, round(self.top_frame.winfo_reqwidth() * .12),
                                            round(self.top_frame.winfo_reqheight()*.5), 5,
-                                           fg_color=Color.White_Ghost, command= show_acc_menubar,
+                                           fg_color=Color.White_Ghost,
                                            hover_color= Color.White_Gray,cursor="hand2")
 
         self.acc_btn.grid(row=0, column= 3, sticky='e', padx=(0,10))
         self.dp = ctk.CTkLabel(self.acc_btn, width * .03, width * .03, 0, 'transparent', 'transparent', text='', image=self.acc_icon,)
         self.dp.grid(row = 0, column = 0, rowspan = 3, sticky = 'nsew', pady = (round(height * .005), 0), padx = (round(height * .01), 0))
-        self.acc_name = ctk.CTkLabel(self.acc_btn, height = 0, fg_color='transparent', text='Juan dela Cruz', font=("Poppins Medium", 15))
+        self.acc_name = ctk.CTkLabel(self.acc_btn, height = 0, fg_color='transparent', text='Maria Leonora Teresa Agoncillo III', font=("Poppins Medium", 15))
         self.acc_name.grid(row = 0, column = 1, sticky = 'sw', padx = (round(height * .005), 0), pady = (5,0))
         self.position = ctk.CTkLabel(self.acc_btn, height = 0, fg_color='transparent', text='Owner', font=("Poppins Medium", 12))
         self.position.grid(row = 1, column = 1, sticky = 'nw', padx = (round(height * .005), 0), pady = 0)
         self.acc_btn.grid(row=0, column= 3, sticky='e', padx=(0,10))
         self.acc_btn.update_children()
+        self.update()
 
+        '''menubars'''
+        self.notif_menu_bar= cctk.menubar(self, width * default_menubar_width, height * default_menubar_height,
+                                          corner_radius= 0, fg_color='black', border_width= 0,
+                                          position=(self.notif_btn.winfo_rootx() / self.winfo_width() + default_menubar_width/2,
+                                                    self.top_frame.winfo_height()/ self.winfo_height() + default_menubar_height/2,
+                                                    'c'))
+        self.settings_menu_bar = cctk.menubar(self, width * default_menubar_width, height * default_menubar_height,
+                                              corner_radius= 5, fg_color=Color.White_Ghost, border_width= 0,
+                                              position=(self.settings_btn.winfo_rootx() / self.winfo_width() + default_menubar_width/2,
+                                                        self.top_frame.winfo_height()/ self.winfo_height() + default_menubar_height/2+0.005,
+                                                        'c'))
+        self.settings_menu_bar_dark_mode = ctk.CTkSwitch(self.settings_menu_bar,width=round(width * default_menubar_width/2), height=round(height * .12),text="Dark Mode",
+                                                         font=("Poppins Medium", 16), progress_color=Color.Blue_LapisLazuli_1, text_color=Color.Blue_Maastricht,
+                                                         variable = switch_var_darkmode, onvalue="darkmode", offvalue="lightmode",
+                                                         command=switch_darkmode,)
+        self.settings_menu_bar_dark_mode.pack(anchor= 'c')
+        self.acc_menu_bar = cctk.menubar(self, width * acc_menubar_width, height * default_menubar_height, 0, fg_color=Color.White_Ghost,
+                                         position= (1 - acc_menubar_width/2,
+                                                    self.top_frame.winfo_height()/ self.winfo_height() + default_menubar_height/2,
+                                                    'c'))
+
+        self.top_frame_button_mngr = cctku.button_manager([self.notif_btn, self.settings_btn, self.acc_btn], '#777777', True,
+                                                          children=[self.notif_menu_bar, self.settings_menu_bar, self.acc_menu_bar])
 
         '''setting default events'''
-        load_main_frame('Dashboard', 0, None)
+        load_main_frame('Dashboard', 0)
         #change_active_event(self.db_button, 0)
         self.mainloop()
 
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''main frames'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 class dashboard_frame(ctk.CTkFrame):
     global width, height
     def __init__(self, master):
         super().__init__(master,corner_radius=0,fg_color=Color.White_Chinese)
-        self.data =[1457,2688]
+        self.data =[69,420]
 
         def show_pie(master):
             labels = ["Items", "Service"]
@@ -464,6 +397,7 @@ class dashboard_frame(ctk.CTkFrame):
         self.log_history_frame = ctk.CTkFrame(self, width=width*.395, height=height*0.395, fg_color=Color.White_Ghost, corner_radius=5)
         self.log_history_frame.grid(row=2, column=4, columnspan=4, padx= (width*(.005) ,width * .01), pady=(height*0.017))
 
+
 class transaction_frame(ctk.CTkFrame):
     global width, height
     def __init__(self, master):
@@ -519,5 +453,9 @@ class histlog_frame(ctk.CTkFrame):
         super().__init__(master,corner_radius=0,fg_color=Color.White_Platinum)
         self.label = ctk.CTkLabel(self, text='9').pack(anchor='w')
         self.grid_forget()
+
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''menu bars'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+#tba
+
 
 dashboard()
