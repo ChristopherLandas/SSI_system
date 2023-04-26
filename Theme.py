f377e5de6591feebed055c0aa599dd12cdac3939
sc1nan <120296782+sc1nan@users.noreply.github.com>
2023-04-27 01:27:07 +0800

    Added inventory status and minor changes (#13)

--- Theme.py
+++ Theme.py
@@ -6,6 +6,7 @@ class Color:
     White_Gray = "#EFEFEF"
     White_Chinese = "#E0E0E0"
     White_AntiFlash = "#F1F1F1"
+    White_SilverSand = "#C5C5C5"
     
     Blue_Oxford = "#0A2647"
     Blue_Maastricht = "#06283D"
@@ -23,5 +24,17 @@ class Color:
     
     Platinum = "#E7E7E7"
     Light_Green = "#97DC86"
+    
     Red_Tulip = "#FD8A8A"
+    Red_Pastel = "#FF6464"
+    
+    Orange_Dandelion = "#FFD966"
+    
+    '''Dark Colors'''
+    
+    Dark_Charcoal = "#313131"
+    Dark_Arsenic = "#414141"
+    Dark_DavyGrey = "#525252"
+    Dark_GraniteGrey = "#626262"
+    Dark_Nickel = "#727272"
     
