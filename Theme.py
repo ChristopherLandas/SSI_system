37015564b28bd8df26e1fa3c144c1879b46defbe
TJoel10 <103483396+TJoel10@users.noreply.github.com>
2023-05-10 21:18:01 +0800

    Minor UI changes (#17)

--- Theme.py
+++ Theme.py
@@ -2,11 +2,13 @@ class Color:
     White_Lotion = "#FCFCFC"
     White_Milk = "#FFFDF6"
     White_Platinum = "#E5E5E5"
-    White_Ghost = ("#F9F9F9", "#020202")
+    White_Ghost = "#F9F9F9"
     White_Gray = "#EFEFEF"
     White_Chinese = "#E0E0E0"
     White_AntiFlash = "#F1F1F1"
     White_SilverSand = "#C5C5C5"
+
+    White_Color = ("#FCFCFC","#FFFDF6","#E5E5E5","#F9F9F9","#EFEFEF","#E0E0E0","#F1F1F1","#C5C5C5",)
     
     Blue_Oxford = "#0A2647"
     Blue_Maastricht = "#06283D"
@@ -30,6 +32,9 @@ class Color:
     
     Orange_Dandelion = "#FFD966"
     
+
+    White_Color = ("#FCFCFC","#FFFDF6","#E5E5E5","#F9F9F9","#EFEFEF","#E0E0E0","#F1F1F1","#C5C5C5",)
+    Red_Color = ("#FD8A8A","#FF6464","#EB455F","#E6556B" )
     '''Dark Colors'''
     
     Dark_Charcoal = "#313131"
