6f3b162007e5c0cb86588a5dd8f717e75ddf5e1a
Christopher Landas <Landas.229246@fairview.sti.edu.ph>
2023-06-02 02:45:31 +0800

    add new widget:info tab

--- customcustomtkinter.py
+++ customcustomtkinter.py
@@ -489,6 +489,19 @@ class customcustomtkinter:
                 self.num_entry.insert(0, self.value)
             self.num_entry.configure(state = 'readonly')
 
+    class info_tab(ctk.CTkFrame):
+        def __init__(self, master: any, width: int = 200, height: int = 200, corner_radius: Optional[Union[int, str]] = None,
+                     border_width: Optional[Union[int, str]] = None, bg_color: Union[str, Tuple[str, str]] = "transparent",
+                     fg_color: Optional[Union[str, Tuple[str, str]]] = None, border_color: Optional[Union[str, Tuple[str, str]]] = None,
+                     background_corner_colors: Union[Tuple[Union[str, Tuple[str, str]]], None] = None,
+                     overwrite_preferred_drawing_method: Union[str, None] = None, tab: ctk.CTkFrame = None,
+                     button_text: str = 'click to edit info', **kwargs):
+            super().__init__(master, width, height, corner_radius, border_width, bg_color, fg_color, border_color,
+                             background_corner_colors, overwrite_preferred_drawing_method, **kwargs)
+            self._tab = tab
+            self._button_text  = button_text
+            self.button = ctk.CTkButton(self, width * .8, height * .8, 12, text = self._button_text)
+            self.button.place(relx = .5, rely = .5, anchor = 'c')
 
 class customcustomtkinterutil:
     class button_manager:
