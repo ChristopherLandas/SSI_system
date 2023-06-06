05957cb10b13ec87af283d570557a13a281ab7a6
Christopher Landas <Landas.229246@fairview.sti.edu.ph>
2023-06-06 08:10:11 +0800

    test

--- tester.py
+++ tester.py
@@ -1,6 +1,6 @@
 import customtkinter as ctk
 from customcustomtkinter import customcustomtkinter as cctk
-from popup import transaction_popups
+from popup import dashboard_popup
 from decimal import Decimal
 
 class body(ctk.CTk):
@@ -11,15 +11,18 @@ class body(ctk.CTk):
         self.attributes("-fullscreen", True)
         self.screen = (self.winfo_screenwidth(), self.winfo_screenheight())
 
-        '''self.infotab = cctk.info_tab(self, self.screen[0] * .8, self.screen[1] * .8)
-        self.infotab._tab = transaction_popups.customer_info(self, self.screen, self.infotab)
-        self.infotab.button.configure(command = lambda: self.infotab._tab.place(relx = .5, rely = .5, anchor = 'c'))
-        self.infotab.place(relx = .5, rely = .5, anchor = 'c')
-'''
-        cctk.cctkTreeView(self, [(1,2,3)], width = self.screen[0] * .9, height = self.screen[1] * .9, column_format='/test:x-iT/t1:x-tc!30!30').place(relx = .5, rely = .5, anchor = 'c')
-
+        test = dashboard_popup.status_bar(self, (250, 75), 'hello', '#121212', 12)
+        test.place(relx = .5, rely = .5, anchor = 'c')
 
         self.mainloop()
 
 
-body()
+#body()
+
+i = [1,3,5,9]
+j = [2,4,6,8]
+
+for (t, s) in (i, j):
+    print(t, s)
+
+#print((s[1], t[1], s[2], t[2] for s, t in i, j))
