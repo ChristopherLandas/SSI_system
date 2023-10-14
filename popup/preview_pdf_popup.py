9258d754211351f6f35d9444d4cbb073bda19bf5
jesserf <famorca.135737@fairview.sti.edu.ph>
2023-10-15 02:31:07 +0800

    initial receipt pdf preview

--- popup/preview_pdf_popup.py
+++ popup/preview_pdf_popup.py
@@ -38,7 +38,7 @@ def browseFiles():
 
 
 
-def generate_report():
+def generate_report(or_number: str, cashier_name: str, client_name: str, pet_name: str, item_particulars, service_particulars, total_amount, amount_paid):
     from reportlab.graphics.shapes import Drawing, Rect, String
     from reportlab.graphics.charts.piecharts import Pie
     from reportlab.pdfgen.canvas import Canvas
@@ -66,6 +66,7 @@ def generate_report():
     today = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
 
     filename = f'image\\sample.pdf'
+    
     pdf = SimpleDocTemplate(
         filename=filename,
         pagesize=letter
@@ -91,7 +92,8 @@ def generate_report():
     second_header_temp = [['SJDM City, Bulacan Hardware 2000 Bldg.'],
                     ['Brgy. Graceville, SJDM City, Bulacan'],
                     ['Tel.: 8994-9043'],
-                    ['Cel.: 0922-976-9287 / 0927-887-0270 /0922-408-7709']]
+                    ['Cel.: 0922-976-9287 / 0927-887-0270 /'],
+                    [' 0922-408-7709']]
     receipt_header2 = Table(second_header_temp)
     header2_style = TableStyle(
         [
@@ -102,25 +104,59 @@ def generate_report():
         ('FONTSIZE', (0, 0), (-1, -1), 12),
         #space at the bottom
         ('BOTTOMPADDING', (0, 0), (-1, -1), -2),
-        ('BOTTOMPADDING', (0, 3), (-1, 3), 15),
+        ('BOTTOMPADDING', (0, 4), (-1, 4), 15),
         ]
     )
     receipt_header.setStyle(header_style)
     receipt_header2.setStyle(header2_style)
+
+    pet_name = None
+    ctr = 0
+    if not service_particulars is None:
+        for p in service_particulars:
+            if ctr > 0:
+                pet_name += ', '
+                pet_name += p[1]
+            else:
+                pet_name = p[1]
+                ctr += 1
+            
     
     receipt_content = [
-        ['STATEMENT OF ACCOUNT', '', f'Date: {today}', ''], 
-        ['OR#:', '', 'Cashier:', ''],
-        ['Name of Client:', '', '', ''],
-        ['Pet Name:', '', '', ''],
-        ['Particulars', 'Quantity', 'Unit Price', 'Amount'],
+        ['Statement of Account', '', f'Date: {today}', ''], 
+        [f'OR#: {or_number}', '', f'Cashier: {cashier_name}', ''],
+        [f'Name of Client: {client_name}', '', '', ''],
+        [f'Pet Name: {pet_name}', '', '', ''],
+        ['Particulars', 'Quantity', 'Unit Price', 'Amount']]
+    ''',
         [' ', '', '', ''],
         [' ', '', '', ''],
         [' ', '', '', ''],
         [' ', '', '', ''],
         [' ', '', '', ''],
         [' ', '', '', ''],
-        ['Total:', '', '', '']]
+        ['Total:', '', '', f'{total_amount}'],
+        ['Amount Paid:', '', '', f'{amount_paid}']]
+    '''
+    if not item_particulars is None:
+        for p in item_particulars:
+            item_prc = "P{:,.2f}".format(float(p[4]))
+            item_total = "P{:,.2f}".format(float(p[4])*float(p[3]))
+            #receipt_content.append([p[2], p[3], p[4], float(p[4])*float(p[3])])
+            receipt_content.append([p[2], p[3], item_prc, item_total])
+    if not service_particulars is None:
+        for p in service_particulars:
+            receipt_content.append([p[0], p[1], f'P{p[6]}', f'P{p[6]}'])
+
+    total_amount_price = "P{:,.2f}".format(float(total_amount))
+    amount_paid_price = "P{:,.2f}".format(float(amount_paid))
+    change_price = "P{:,.2f}".format(float(amount_paid)-float(total_amount))
+    #receipt_content.append(['Total:', '', '', f'{total_amount}'])
+    #receipt_content.append(['Amount Paid:', '', '', f'{amount_paid}'])
+    #receipt_content.append(['Change:', '', '', f'{float(amount_paid)}-{float(total_amount)}'])
+    receipt_content.append(['Total:', '', '', f'{total_amount_price}'])
+    receipt_content.append(['Amount Paid:', '', '', f'{amount_paid_price}'])
+    receipt_content.append(['Change:', '', '', f'{change_price}'])
 
     #add data for table
     
@@ -136,10 +172,15 @@ def generate_report():
         ('SPAN', (2, 1), (-1, 1)),
         ('SPAN', (0, 2), (-1, 2)),
         ('SPAN', (0, 3), (-1, 3)),
-        ('SPAN', (0, 11), (2, 11)),
+        ('SPAN', (0, len(receipt_content)-3), (2, len(receipt_content)-3)),
+        ('SPAN', (0, len(receipt_content)-2), (2, len(receipt_content)-2)),
+        ('SPAN', (0, len(receipt_content)-1), (2, len(receipt_content)-1)),
         ('BACKGROUND', (0, 4), (-1, 4), bc),
         ('ALIGN', (0, 1), (0, -1), 'LEFT'),
-        ('ALIGN', (0, 11), (0, 11), 'RIGHT'),
+        ('ALIGN', (0, len(receipt_content)-1), (0, len(receipt_content)-1), 'RIGHT'),
+        ('ALIGN', (0, len(receipt_content)-2), (0, len(receipt_content)-2), 'RIGHT'),
+        ('ALIGN', (0, len(receipt_content)-3), (0, len(receipt_content)-3), 'RIGHT'),
+        ('ALIGN', (1, 5), (3, len(receipt_content)-1), 'RIGHT'),
         #font style
         ('FONTNAME', (0, 0), (0, 0), 'Times-New-Roman-Bold'),
         ('FONTNAME', (0, 1), (-1, -1), 'Times-New-Roman'),
@@ -175,11 +216,11 @@ def generate_report():
     #add borders
     ts = TableStyle([
         ('GRID', (0, 0), (-1, 4), 0.5, colors.black),
-        ('GRID', (0, 11), (-1, 11), 0.5, colors.black),
-        ('BOX', (0, 5), (0, -1), 0.5, colors.black),
-        ('BOX', (1, 5), (1, -1), 0.5, colors.black),
-        ('BOX', (2, 5), (2, -1), 0.5, colors.black),
-        ('BOX', (3, 5), (3, -1), 0.5, colors.black),
+        ('GRID', (0, len(receipt_content)-3), (-1, len(receipt_content)-1), 0.5, colors.black),
+        ('BOX', (0, 5), (0, len(receipt_content)-4), 0.5, colors.black),
+        ('BOX', (1, 5), (1, len(receipt_content)-4), 0.5, colors.black),
+        ('BOX', (2, 5), (2, len(receipt_content)-4), 0.5, colors.black),
+        ('BOX', (3, 5), (3, len(receipt_content)-4), 0.5, colors.black),
     ])
     table_content.setStyle(ts)
     
@@ -207,6 +248,189 @@ def generate_report():
     writer = pdfrw2()
     writer.write(filename, p1)
 
+    #region none sample
+
+    filename = f'image\\receipt_{or_number}.pdf'
+    
+    pdf = SimpleDocTemplate(
+        filename=filename,
+        pagesize=letter
+    )
+    #header
+    receipt_header_temp = [['JOSEPH Z. ANGELES VETERINARY CLINIC'],
+                    ['LIVESTOCK CONSULTANCY']]
+    receipt_header = Table(receipt_header_temp)
+    header_style = TableStyle(
+        [
+        #text alignment, starting axis, -1 = end
+        ('ALIGN', (0, 0), (0, -1), 'CENTER'),
+        #font style
+        ('FONTNAME', (0, 0), (-1, -1), 'Times-New-Roman-Bold'),
+        ('FONTSIZE', (0, 0), (-1, -1), 18),
+        #space at the bottom
+        ('BOTTOMPADDING', (0, 0), (0, 0), -30),
+        ('BOTTOMPADDING', (0, 0), (0, 1), 10),
+        ('TOPPADDING', (0, 1), (0, 1), -5),
+        ]
+    )
+
+    second_header_temp = [['SJDM City, Bulacan Hardware 2000 Bldg.'],
+                    ['Brgy. Graceville, SJDM City, Bulacan'],
+                    ['Tel.: 8994-9043'],
+                    ['Cel.: 0922-976-9287 / 0927-887-0270 /'],
+                    [' 0922-408-7709']]
+    receipt_header2 = Table(second_header_temp)
+    header2_style = TableStyle(
+        [
+        #text alignment, starting axis, -1 = end
+        ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+        #font style
+        ('FONTNAME', (0, 0), (-1, -1), 'Times-New-Roman'),
+        ('FONTSIZE', (0, 0), (-1, -1), 12),
+        #space at the bottom
+        ('BOTTOMPADDING', (0, 0), (-1, -1), -2),
+        ('BOTTOMPADDING', (0, 4), (-1, 4), 15),
+        ]
+    )
+    receipt_header.setStyle(header_style)
+    receipt_header2.setStyle(header2_style)
+
+    pet_name = None
+    ctr = 0
+    if not service_particulars is None:
+        for p in service_particulars:
+            if ctr > 0:
+                pet_name += ', '
+                pet_name += p[1]
+            else:
+                pet_name = p[1]
+                ctr += 1
+            
+    
+    receipt_content = [
+        ['Statement of Account', '', f'Date: {today}', ''], 
+        [f'OR#: {or_number}', '', f'Cashier: {cashier_name}', ''],
+        [f'Name of Client: {client_name}', '', '', ''],
+        [f'Pet Name: {pet_name}', '', '', ''],
+        ['Particulars', 'Quantity', 'Unit Price', 'Amount']]
+    ''',
+        [' ', '', '', ''],
+        [' ', '', '', ''],
+        [' ', '', '', ''],
+        [' ', '', '', ''],
+        [' ', '', '', ''],
+        [' ', '', '', ''],
+        ['Total:', '', '', f'{total_amount}'],
+        ['Amount Paid:', '', '', f'{amount_paid}']]
+    '''
+    if not item_particulars is None:
+        for p in item_particulars:
+            item_prc = "P{:,.2f}".format(float(p[4]))
+            item_total = "P{:,.2f}".format(float(p[4])*float(p[3]))
+            receipt_content.append([p[2], p[3], item_prc, item_total])
+    if not service_particulars is None:
+        for p in service_particulars:
+            receipt_content.append([p[0], p[1], f'P{p[6]}', f'P{p[6]}'])
+
+    total_amount_price = "P{:,.2f}".format(float(total_amount))
+    amount_paid_price = "P{:,.2f}".format(float(amount_paid))
+    change_price = "P{:,.2f}".format(float(amount_paid)-float(total_amount))
+    receipt_content.append(['Total:', '', '', f'{total_amount_price}'])
+    receipt_content.append(['Amount Paid:', '', '', f'{amount_paid_price}'])
+    receipt_content.append(['Change:', '', '', f'{change_price}'])
+
+    #add data for table
+    
+    table_content = Table(receipt_content)
+    bc = colors.lightgrey
+    #add table style
+    tbl_style = TableStyle(
+        [
+        #text alignment, starting axis, -1 = end
+        ('SPAN', (0, 0), (1, 0)),
+        ('SPAN', (2, 0), (-1, 0)),
+        ('SPAN', (0, 1), (1, 1)),
+        ('SPAN', (2, 1), (-1, 1)),
+        ('SPAN', (0, 2), (-1, 2)),
+        ('SPAN', (0, 3), (-1, 3)),
+        ('SPAN', (0, len(receipt_content)-3), (2, len(receipt_content)-3)),
+        ('SPAN', (0, len(receipt_content)-2), (2, len(receipt_content)-2)),
+        ('SPAN', (0, len(receipt_content)-1), (2, len(receipt_content)-1)),
+        ('BACKGROUND', (0, 4), (-1, 4), bc),
+        ('ALIGN', (0, 1), (0, -1), 'LEFT'),
+        ('ALIGN', (0, len(receipt_content)-1), (0, len(receipt_content)-1), 'RIGHT'),
+        ('ALIGN', (0, len(receipt_content)-2), (0, len(receipt_content)-2), 'RIGHT'),
+        ('ALIGN', (0, len(receipt_content)-3), (0, len(receipt_content)-3), 'RIGHT'),
+        ('ALIGN', (1, 5), (3, len(receipt_content)-1), 'RIGHT'),
+        #font style
+        ('FONTNAME', (0, 0), (0, 0), 'Times-New-Roman-Bold'),
+        ('FONTNAME', (0, 1), (-1, -1), 'Times-New-Roman'),
+        ('FONTSIZE', (0, 0), (-1, -1), 10),
+        ]
+    )
+
+    table_content.setStyle(tbl_style)
+    #alternate background color
+    rowNumb = len(receipt_content)
+    for i in range(1, rowNumb):
+        if i % 2 == 0:
+            bc = colors.white
+        else:
+            bc = colors.lightgrey
+
+        ts = TableStyle(
+            [('BACKGROUND', (0, i), (-1, i), bc)]
+        )
+        #table_content.setStyle(ts)
+
+    rowNumb = len(receipt_content)
+    for i in range(1, rowNumb):
+        if i % 2 == 0:
+            bc = colors.white
+        else:
+            bc = colors.lightgrey
+
+        ts = TableStyle(
+            [('BACKGROUND', (0, i), (-1, i), bc)]
+        )
+
+    #add borders
+    ts = TableStyle([
+        ('GRID', (0, 0), (-1, 4), 0.5, colors.black),
+        ('GRID', (0, len(receipt_content)-3), (-1, len(receipt_content)-1), 0.5, colors.black),
+        ('BOX', (0, 5), (0, len(receipt_content)-4), 0.5, colors.black),
+        ('BOX', (1, 5), (1, len(receipt_content)-4), 0.5, colors.black),
+        ('BOX', (2, 5), (2, len(receipt_content)-4), 0.5, colors.black),
+        ('BOX', (3, 5), (3, len(receipt_content)-4), 0.5, colors.black),
+    ])
+    table_content.setStyle(ts)
+    
+    elems = []
+    elems.append(receipt_header)
+    elems.append(receipt_header2)
+    elems.append(table_content)
+
+    pdf.build(elems)
+    #pdf compilation
+    merger = PdfWriter()
+    input2 = open(filename, "rb")
+    
+    # add the first 3 pages of input1 document to output
+    merger.append(input2)
+    # Write to an output PDF document
+    output = open(filename, "wb")
+    merger.write(output)
+    # Close File Descriptors
+    merger.close()
+    output.close()
+    #add footer
+    p1 = pdfrw1(filename)
+
+    writer = pdfrw2()
+    writer.write(filename, p1)
+
+    #endregion
+
 class ShowPdf(pdf.ShowPdf):
     def goto(self, page):
         try:
@@ -215,7 +439,7 @@ class ShowPdf(pdf.ShowPdf):
             if self.img_object_li:
                 self.text.see(self.img_object_li[-1])
 
-def preview_pdf_popup(receipt: int):
+def preview_pdf_popup(receipt: int, ornum = None, cashier = None, client = None, pet = None, item = None, service = None, total = None, paid = None):
     previewpopup = ctk.CTkToplevel()
     previewpopup.attributes('-topmost', 1)
     previewpopup.geometry('630x700+400+100')
@@ -224,7 +448,7 @@ def preview_pdf_popup(receipt: int):
 
     pdfviewer = ShowPdf()
     if receipt:
-        generate_report()
+        generate_report(or_number = ornum, cashier_name = cashier, client_name = client, pet_name = pet, item_particulars = item, service_particulars = service, total_amount = total, amount_paid = paid)
     filename='image/sample.pdf'
     vaas2 = NONE
     vaas1=pdf.ShowPdf()
