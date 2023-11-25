50d39d968766993d01e283dc27bccd8f95971dd3
Christopher Landas <Landas.229246@fairview.sti.edu.ph>
2023-11-25 23:54:05 +0800

    test

--- test.py
+++ test.py
@@ -1,18 +1,13 @@
-import os
-import re
-
-def get_ip():
-    file=os.popen("ipconfig")
-    data=file.read()
-    file.close()
-    bits=[s.strip() for s in data.strip().split('\n')]
-    
-    for i in range(len(bits)):
-        if 'Default' in bits[i]:
-            li1 = re.search(r'^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])$', bits[i][bits[i].index(': ')+2:])
-            li2 = re.search(r'^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])$', bits[i+1])
-            if li1 is not None:
-                return li1[0]
-            if li2 is not None:
-                return bits[i+1]
-print(get_ip())
+import subprocess 
+Data = subprocess.check_output(['wmic', 'product', 'get', 'name']) 
+a = str(Data) 
+try: 
+    for i in range(len(a)): 
+        app = a.split("\\r\\r\\n")[6:][i]
+        if 'MariaDB' in app:
+            if float(app.split(' ')[1]) < 11:
+                print('Unable to proceed')
+            else:
+                break
+except IndexError as e: 
+    print("All Done")
